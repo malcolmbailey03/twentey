5af27e4a7e1b0683c3fcf9807276f1157bfa9aa7
Bailey <mbailey2@FVFZMYHCJ1WK.tld>
2022-03-21 10:54:21 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+alert("welcome to 20! This is a competition to see whitch player gets closer number 20.you are going to be playing ageinst the computer . you will bw drawing numbers 1-10 the computer will stop a 16. pay attion to the screen so you know what your number is )
+
+alert("Now... its the computers turn")
+
+var computer = 0;
+
+var human = 0;
+
+do {
+
+    var ran= Math.floor(Math.random() * 10) +1
+
+    computer = computer + ran
+
+    alert(`Computer new number is ${ran}. 
+    
+    Computer new total is ${computer}.`)
+
+    if (computer >= 20) break
+
+
+
+
+
+} while ( computer < 16)
+
+
+
+do {
+
+   var 
+
+
+
+
+} while 
